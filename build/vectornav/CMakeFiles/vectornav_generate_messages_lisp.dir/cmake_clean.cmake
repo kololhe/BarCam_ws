c34e0f8420192ffbc9e0bad8f00d5dfff377cd8e
Shaunak Kolhe <118778835+kololheUSL@users.noreply.github.com>
2023-06-09 16:41:05 -0500

    Delete build directory

--- build/vectornav/CMakeFiles/vectornav_generate_messages_lisp.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "/home/kololhe/BarCam_ws/devel/share/common-lisp/ros/vectornav/msg/Ins.lisp"
-  "CMakeFiles/vectornav_generate_messages_lisp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/vectornav_generate_messages_lisp.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
